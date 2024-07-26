a37fe54c1b60621103f8bd091150b65d9307bbef
soundarraj02 <soundarraj1271@gmail.com>
2024-07-26 13:54:38 +0530

    initial commit

--- db.js
+++ db.js
@@ -1,12 +1,10 @@
 const mongoose = require("mongoose");
-const connectWithDb = () => {
-    mongoose.connect("mongodb+srv://soundarraj1271:JbGuZvYk0WR13kiR@soundhar.gakg0r8.mongodb.net/task-1");
 
-    const db = mongoose.connection;
-    db.on("error", console.error.bind(console, "connection error:"));
-    db.once("open", () => {
-        console.log("Database connected");
-    });
-};
+const connectWithDb = () => {
+mongoose.connect(
+    "mongodb+srv://soundarraj1271:JbGuZvYk0WR13kiR@soundhar.gakg0r8.mongodb.net/task-1",
+)
+.then(() => console.log('DB Connected'))
+}
 
 module.exports = {connectWithDb};
